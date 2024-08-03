c1dd8c53037045b9d92496c13b3f038e0aacce06
Hybola <keeratipong.b@gmail.com>
2024-08-03 14:57:22 +0700

    lecture 290: Fetching Orders

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,47 +1,52 @@
 // Test ID: IIDSAT
+// Test ID2: CQE92U
 
+import { useLoaderData } from "react-router-dom";
 import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
 } from "../../utils/helpers";
+import { getOrder } from "../../services/apiRestaurant";
 
-const order = {
-  id: "ABCDEF",
-  customer: "Jonas",
-  phone: "123456789",
-  address: "Arroios, Lisbon , Portugal",
-  priority: true,
-  estimatedDelivery: "2027-04-25T10:00:00",
-  cart: [
-    {
-      pizzaId: 7,
-      name: "Napoli",
-      quantity: 3,
-      unitPrice: 16,
-      totalPrice: 48,
-    },
-    {
-      pizzaId: 5,
-      name: "Diavola",
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-    {
-      pizzaId: 3,
-      name: "Romana",
-      quantity: 1,
-      unitPrice: 15,
-      totalPrice: 15,
-    },
-  ],
-  position: "-9.000,38.000",
-  orderPrice: 95,
-  priorityPrice: 19,
-};
+// const order = {
+//   id: "ABCDEF",
+//   customer: "Jonas",
+//   phone: "123456789",
+//   address: "Arroios, Lisbon , Portugal",
+//   priority: true,
+//   estimatedDelivery: "2027-04-25T10:00:00",
+//   cart: [
+//     {
+//       pizzaId: 7,
+//       name: "Napoli",
+//       quantity: 3,
+//       unitPrice: 16,
+//       totalPrice: 48,
+//     },
+//     {
+//       pizzaId: 5,
+//       name: "Diavola",
+//       quantity: 2,
+//       unitPrice: 16,
+//       totalPrice: 32,
+//     },
+//     {
+//       pizzaId: 3,
+//       name: "Romana",
+//       quantity: 1,
+//       unitPrice: 15,
+//       totalPrice: 15,
+//     },
+//   ],
+//   position: "-9.000,38.000",
+//   orderPrice: 95,
+//   priorityPrice: 19,
+// };
 
 function Order() {
+  const order = useLoaderData();
+
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
@@ -52,6 +57,7 @@ function Order() {
     estimatedDelivery,
     cart,
   } = order;
+
   const deliveryIn = calcMinutesLeft(estimatedDelivery);
 
   return (
@@ -81,6 +87,9 @@ function Order() {
       </div>
     </div>
   );
+}
+export async function loader({ params }) {
+  return await getOrder(params.orderId);
 }
 
 export default Order;
