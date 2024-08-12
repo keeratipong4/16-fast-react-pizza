ca224a660566bf04a6356db80213568f774c7c4c
Hybola <keeratipong.b@gmail.com>
2024-08-12 17:24:27 +0700

    lecture 313,314

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,7 @@
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -34,6 +35,7 @@ const fakeCart = [
 
 function CreateOrder() {
   const navigation = useNavigation();
+  const username = useSelector((state) => state.user.username);
   const isSubmiting = navigation.state === "submitting";
   const formError = useActionData();
   // const [withPriority, setWithPriority] = useState(false);
@@ -49,7 +51,13 @@ function CreateOrder() {
       <Form method="POST" action="/order/new">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input grow" type="text" name="customer" required />
+          <input
+            className="input grow"
+            type="text"
+            name="customer"
+            defaultValue={username}
+            required
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,6 +1,9 @@
+import { useSelector } from "react-redux";
+
 function Username() {
+  const username = useSelector((state) => state.user.username);
   return (
-    <div className="hidden text-sm font-semibold md:block">Keeratipong</div>
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
   );
 }
 
