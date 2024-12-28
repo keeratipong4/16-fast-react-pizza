356e929dcdb0e0dbad9bc30705850b9ffa0f83e2
Keeratipong <keeratipong.b@gmail.com>
2024-12-29 01:57:20 +0700

    add pizza logo into header and a cart into CartOverview

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { getTotalCartPrice, getTotalCartQuatity } from "./cartSlice";
 import { formatCurrency } from "../../utils/helpers";
+import CartIcon from "../../utils/CartIcon";
 
 function CartOverview() {
   const totalCartQuantity = useSelector(getTotalCartQuatity);
@@ -11,9 +12,17 @@ function CartOverview() {
     <div className="flex items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
       <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
         <span>{totalCartQuantity} pizzas</span>
-        <span>{formatCurrency(totalCartPrice)}</span>
+        <span className="text-yellow-400">
+          {formatCurrency(totalCartPrice)}
+        </span>
       </p>
-      <Link to="/cart">Open cart &rarr;</Link>
+
+      <Link className="flex items-center gap-2" to="/cart">
+        <span>
+          <CartIcon />
+        </span>
+        Open cart &rarr;
+      </Link>
     </div>
   );
 }
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
 import Username from "../features/user/Username";
+import PizzaIcon from "../utils/PizzaIcon";
 
 function Header() {
   return (
     <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
-      <Link to="/" className="tracking-widest">
+      <Link to="/" className="flex items-center gap-1 tracking-widest">
         Fast React Pizza.Co
+        {/* <span className="text-3xl"> 🍕</span> */}
+        <span className="h-10 w-10">
+          <PizzaIcon />
+        </span>
       </Link>
       <SearchOrder />
       <Username />
