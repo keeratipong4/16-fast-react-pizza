536ba7c21fb9764e08669e8f30f0a6708a123c36
Keeratipong <keeratipong.b@gmail.com>
2024-12-29 23:50:21 +0700

    edit <EmptyCart/>

--- src/features/cart/EmptyCart.jsx
+++ src/features/cart/EmptyCart.jsx
@@ -1,13 +1,19 @@
 import LinkButton from "../../ui/LinkButton";
+import { CartEmptyIcon } from "../../utils/Icons";
 
 function EmptyCart() {
   return (
-    <div className="px3 py-4">
+    <div className="px3 mx-5 py-4">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
-
-      <p className="mt-7 font-semibold">
-        Your cart is still empty. Start adding some pizzas :)
-      </p>
+      <div className="mt-7 flex flex-col gap-3">
+        <div className="mx-auto">
+          <CartEmptyIcon />
+        </div>
+        <p className="mx-auto font-semibold">
+          Your cart is still empty. <br />
+          Start adding some pizzas :)
+        </p>
+      </div>
     </div>
   );
 }
--- src/utils/Icons.jsx
+++ src/utils/Icons.jsx
@@ -122,3 +122,31 @@ export function CartIcon({ size = 30, ...props }) {
     </svg>
   );
 }
+export const CartEmptyIcon = ({ size = 50, ...props }) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <path
+          d="M21 5L19 12H7.37671M20 16H8L6 3H3M11 3L13.5 5.5M13.5 5.5L16 8M13.5 5.5L16 3M13.5 5.5L11 8M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z"
+          stroke="#facc15"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>{" "}
+      </g>
+    </svg>
+  );
+};
