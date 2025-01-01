7a29334cc1d41fa61f38b14aa435547299177fcc
Keeratipong <keeratipong.b@gmail.com>
2025-01-01 16:56:49 +0700

    update header text no-wrap

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -6,9 +6,11 @@ import { PizzaIcon } from "../utils/Icons";
 function Header() {
   return (
     <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
-      <Link to="/" className="flex items-center gap-1 sm:tracking-widest">
+      <Link
+        to="/"
+        className="flex items-center gap-1 text-nowrap sm:tracking-widest"
+      >
         Fast React Pizza.Co
-        {/* <span className="text-3xl"> 🍕</span> */}
         <span className="h-10 w-10">
           <PizzaIcon />
         </span>
